c7b77a6f20e6aee93fcd7d71e413131020f257e6
Gabriel Ferreyra <ferreyraga@gmail.com>
2020-06-29 22:41:09 -0300

    version 4

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ let propiedadesThemes =
         {//Sailor Day = 0
             colorFondoBody: "rgba(247, 201, 243,0.2)",
             gradienteTheme: "linear-gradient(270deg, #F7C9F3 0%, #4180F6 100%)",
-            gifOfLogo: "./assets/gifOF_logo.png",
+            gifOfLogo: "/assets/gifOF_logo.png",
             colorFontBotonesRosa: "#110038",
             colorBotonRosa: "#F7C9F3",
             colorBotonRosaHover: '#E6BBE2',
@@ -50,7 +50,7 @@ let propiedadesThemes =
         {//Sailor Night = 1
             colorFondoBody: "rgba(19, 15, 64,1.0)",
             gradienteTheme: "linear-gradient(270deg, #EE3EFE 0%, #2E32FB 100%)",
-            gifOfLogo: "./assets/gifOF_logo_dark.png",
+            gifOfLogo: "/assets/gifOF_logo_dark.png",
             colorFontBotonesRosa: "#FFFFFF",
             colorBotonRosa: "#EE3EFE",
             colorBotonRosaHover: '#CE36DB',
